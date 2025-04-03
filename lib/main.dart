902a61be07cf512426e234ea06310b673caef1c0
abdelrahman yasser <manamano20003@gmaile.com>
2025-04-03 04:58:53 -0700

    new edits2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-
-
-
 import 'package:y2y/provider/project.dart';
 import 'package:y2y/ui/Screens/authentication/reset_Password_Screen.dart';
 import 'package:y2y/ui/Screens/authentication/verify_email.dart';
